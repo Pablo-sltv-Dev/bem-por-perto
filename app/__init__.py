10882ac9662d6612fdcb36fcce0b5b0270a0c86e
Pablo-sltv-Dev <pablo.pmlf@gmail.com>
2025-12-12 15:48:19 -0300

    atualização feita

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
-# Entenda esse arquivo como se fosse a cozinha
-# a demais pastas seria o armazem aonde se encontra os ingredientes
-from flask import Flask
-# from flask_mysqldb import MySQL
-# from dotenv import load_dotenv
-# import os
+from flask import Flask, jsonify
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.wrappers import Response
 
 # carrega as variáveis do .env (senha, banco etc.)
 # load_dotenv()
@@ -16,5 +13,7 @@ app.secret_key = "ksksks"
 
 from app.routes import * #esse seria o caminho para poder chegar até os ingredientes
 # from .models import models
+
+
 
 
--- app/routes/pg_for.py
+++ app/routes/pg_for.py
@@ -1,5 +1,5 @@
 
-from flask import render_template, url_for
+from flask import render_template
 from app import app
 
 @app.route("/cadastro")
